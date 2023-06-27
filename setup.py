71e984abc25d6e307c6f1876fb3c2f8975ea759e
Ton Diaz <tondiaz@gmail.com>
2023-06-27 19:25:32 +0800

    0627

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='psalpsdtools',
-      version='0.5',
+      version='0.6',
       description='PSA-LPSD Tools',
       packages=['psalpsdtools'],
       author_email='a.diaziii@psa.gov.ph',
